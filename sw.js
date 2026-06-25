887caa7cda7bd06255798c49f706d8117c4918b9
basaam68401-spec <basaam68401@gmail.com>
2026-06-25 20:11:51 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='planner-1405-supabase-v2';
+const CACHE='planner-1405-supabase-v4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./supabase-config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
